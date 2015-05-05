7e400bbe7000a52f05b02a89ca83536be3c67b5d
Tianxiang Chen <txchendev@gmail.com>
2015-05-05 10:59:29 -0700

    entry point use es5

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import app from './app'
+// entry point, do not use babel syntax here, so that node --harmony can launch this.
+// nodemon --harmony is a little bit faster than nodemon --exec 'babel-node --harmony'
+require("babel/register")
 
-let port = 3000
+// ES5 code here
+var app = require('./app')
+
+var port = 3000
 app.listen(port, () => {
-  console.log(`listening on http://localhost:${port}`)
+  console.log('listening on http://localhost:' + port)
 })
